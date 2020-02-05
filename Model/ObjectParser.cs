a0ea1883988381625ef3f2bcbf26571399494ec0
kikoo <21300321@handong.edu>
2020-02-05 11:51:32 +0900

    NewFrontAndViewModelMerge

---
+++ Model/ObjectParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SV_final.Model
+{
+    public class ObjectParser
+    {
+        public void readTxt(string filename)
+        {
+            string txtFile = File.ReadAllText(filename);
+
+
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV_final.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        public OpenViewModel OpenVM { get; set; }
+
+        private object _openContent;
+
+        public object OpenContent
+        {
+            get { return _openContent; }
+            set
+            {
+                _openContent = value;
+                OnPropertyChanged("Content1");
+            }
+        }
+
+        public MainViewModel()
+        {
+            OpenVM = new OpenViewModel(this);
+            this.OpenContent = new OpenViewModel(this);
+        }
+    }
+}

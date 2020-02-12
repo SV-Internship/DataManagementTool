bb126894a91cfe6d393dd26c6f7a46872aaeb223
kikoo <21300321@handong.edu>
2020-02-12 15:39:27 +0900

    chamjin Image Change

---
+++ ViewModel/ImgChangeViewModel.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
+using System.Drawing;
+
+namespace SV_final.ViewModel
+{
+    class ImgChangeViewModel : BaseViewModel
+    {
+        private ObservableCollection<string> _fileList;
+
+        public ObservableCollection<string> FileList
+        {
+            get { return _fileList; }
+            set
+            {
+                _fileList = value;
+                OnPropertyChanged("FileList");
+            }
+        }
+
+        private ObservableCollection<string> _visibleFileList;
+
+        public ObservableCollection<string> VisibleFileList
+        {
+            get { return _visibleFileList; }
+            set
+            {
+                _visibleFileList = value;
+                OnPropertyChanged("VisibleFileList");
+            }
+        }
+
+        private string _destpath;
+
+        public string DestPath
+        {
+            get { return _destpath; }
+            set
+            {
+                _destpath = value;
+                OnPropertyChanged("DestPath");
+            }
+        }
+
+        private string _imgSelect;
+
+        public string ImgSelect
+        {
+            get { return _imgSelect; }
+            set
+            {
+                _imgSelect = value;
+                Console.WriteLine(_imgSelect);
+                OnPropertyChanged("ImgSelect");
+            }
+        }
+
+        public RelayCommand ImgOpenCommand { get; private set; }
+        public RelayCommand WholeImgConvertCommand { get; private set; }
+
+        public RelayCommand SelectImgConvertCommand { get; private set; }
+
+        public ImgChangeViewModel()
+        {
+            FileList = new ObservableCollection<string>();
+            VisibleFileList = new ObservableCollection<string>();
+            ImgOpenCommand = new RelayCommand(ImgOpen);
+            WholeImgConvertCommand = new RelayCommand(WholeImgConvert);
+            SelectImgConvertCommand = new RelayCommand(SelectImgConvert);
+        }
+
+        private void ImgOpen()
+        {
+            string ImgFilePath = "";
+
+            try
+            {
+                var dialog = new OpenFileDialog();
+                dialog.ValidateNames = false;
+                dialog.CheckFileExists = false;
+                dialog.CheckPathExists = true;
+                dialog.FileName = "default";
+                dialog.Filter = "All files|*.*|Image files(*.jpg)|*.jpg";
+                dialog.ShowDialog();
+
+                string Path = System.IO.Path.GetFullPath(dialog.FileName);
+                Path = Path.Replace("default", "").Trim();
+                ImgFilePath = Path;
+
+                string[] parser = Path.Split('.');
+                Console.WriteLine(Path);
+
+                if(parser.Length == 2)
+                {
+                    string imgpath = Path;
+                    FileList.Add(imgpath);
+                    string temp = imgpath;
+                    string[] fileparse = temp.Split('\\');
+                    VisibleFileList.Add(fileparse[fileparse.Length - 1]);
+                }
+                
+                else
+                {
+                    string[] parse = Path.Split('.');
+                    string[] files = Directory.GetFiles(parse[0]);
+
+                    foreach(string x in files)
+                    {
+                        FileList.Add(x);
+                        string temp = x;
+                        string[] fileparse = temp.Split('\\');
+                        VisibleFileList.Add(fileparse[fileparse.Length - 1]);
+                    }
+                }
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An exception occurred from {MethodBase.GetCurrentMethod().Name}");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void WholeImgConvert()
+        {
+            foreach(string x in FileList)
+            {
+                Image target = Image.FromFile(x);
+                string finaldest = DestPath + VisibleFileList[FileList.IndexOf(x)];
+                string[] fileparse = finaldest.Split('.');
+                Bitmap bmp = new Bitmap(target);
+                if (target != null)
+                    bmp.Save(DestPath + fileparse[1] + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            }
+        }
+
+        private void SelectImgConvert()
+        {
+            Image target = Image.FromFile(FileList[VisibleFileList.IndexOf(ImgSelect)]);
+            string finaldest = DestPath + VisibleFileList[VisibleFileList.IndexOf(ImgSelect)];
+            string[] fileparse = finaldest.Split('.');
+            Bitmap bmp = new Bitmap(target);
+            if(target != null)
+                bmp.Save(DestPath + fileparse[1] + ".png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+    }
+}
--- ViewModel/MergeViewModel.cs
+++ ViewModel/MergeViewModel.cs
@@ -1,19 +1,168 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using SV_final.Model;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Serialization;
 
 namespace SV_final.ViewModel
 {
-    class MergeViewModel :BaseViewModel
+    class MergeViewModel : BaseViewModel
     {
         private LogViewModel logViewModel;
+        private MainViewModel mainViewModel;
+        private OpenViewModel openViewModel;
+        public ICommand OpenPathCommand { get; private set; }
+        public ICommand SavePathCommand { get; private set; }
+        public ICommand LoadCommand { get; private set; }
+        public ICommand DeleteCommand { get; private set; }
 
-        public MergeViewModel()
+        public ObservableCollection<ObjectDetect> MergeList { get => _mergeList; set => _mergeList = value; }
+        private ObservableCollection<ObjectDetect> _mergeList;
+
+        public ObservableCollection<string> FileNameList { get => _fileNameList; set => _fileNameList = value; }
+        private ObservableCollection<string> _fileNameList;
+
+        public string SelectedItem { get; set; }
+
+        private string _outPath;
+        public string OutPath
         {
-
+            get
+            {
+                return _outPath;
+            }
+            set
+            {
+                _outPath = value;
+                OnPropertyChanged("OutPath");
+            }
         }
 
+        public MergeViewModel(MainViewModel mainViewModel, LogViewModel logViewModel, OpenViewModel openViewModel)
+        {
+            this.mainViewModel = mainViewModel;
+            this.logViewModel = logViewModel;
+            this.openViewModel = openViewModel;
+            OpenPathCommand = new RelayCommand(OpenPath);
+            SavePathCommand = new RelayCommand(SavePath);
+            LoadCommand = new RelayCommand(Load);
+            DeleteCommand = new RelayCommand(Delete);
+            MergeList = new ObservableCollection<ObjectDetect>();
+            FileNameList = new ObservableCollection<string>();
+        }
+
+        private void DoMerge()
+        {
+            ObjectDetect NewOD = new ObjectDetect();
+            foreach (ObjectDetect OriOD in MergeList)
+            {
+                NewOD.Files.File.AddRange(OriOD.Files.File);
+            }
+
+            NewOD.Files.FileCount = NewOD.Files.File.Count();
+
+            ODToXml(NewOD, OutPath);
+
+            logViewModel.AddLog(GetType(), OutPath);
+        }
+
+        private ObjectDetect XmlToOD(string fileName)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(ObjectDetect));
+            TextReader reader = new StreamReader(@fileName);
+
+            ObjectDetect result = (ObjectDetect)deserializer.Deserialize(reader);
+            reader.Close();
+            return result;
+        }
+
+        private void ODToXml(ObjectDetect OD, string fileName)
+        {
+            var writer = new XmlSerializer(typeof(ObjectDetect));
+            var wfile = new System.IO.StreamWriter(fileName);
+            writer.Serialize(wfile, OD);
+            wfile.Close();
+        }
+
+        private void OpenPath()
+        {
+            try
+            {
+                var dlg = new CommonOpenFileDialog();
+                dlg.Filters.Add(new CommonFileDialogFilter("xml", "xml"));
+                if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    OutPath = dlg.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred from {MethodBase.GetCurrentMethod().Name}");
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+        private void SavePath()
+        {
+            try
+            {
+                DoMerge();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred from {MethodBase.GetCurrentMethod().Name}");
+                Console.WriteLine(ex.ToString());
+                logViewModel.FailLog(GetType(), OutPath + "저장에 실패하였습니다.");
+            }
+        }
+        private void Load()
+        {
+            try
+            {
+                MergeList.Clear();
+                foreach (ObjectDetect MainOD in mainViewModel.ListData)
+                {
+                    MergeList.Add(MainOD);
+                }
+                FileNameList.Clear();
+                foreach (string OpenF in openViewModel.VisualFileList)
+                {
+                    FileNameList.Add(OpenF);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred from {MethodBase.GetCurrentMethod().Name}");
+                Console.WriteLine(ex.ToString());
+                logViewModel.FailLog(GetType(), OutPath + "로드에 실패하였습니다.");
+            }
+        }
+        private void Delete()
+        {
+            try
+            {
+                if (SelectedItem != null)
+                {
+                    MergeList.RemoveAt(FileNameList.IndexOf(SelectedItem));
+                    FileNameList.Remove(SelectedItem);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An exception occurred from {MethodBase.GetCurrentMethod().Name}");
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }
